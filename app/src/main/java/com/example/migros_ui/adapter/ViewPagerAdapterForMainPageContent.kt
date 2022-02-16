de10918f82c59431b2aa291b6d665c6fb4359931
Burak <design.burakk@gmail.com>
2022-02-16 15:34:48 +0300

    ViewPagerAdapterForMainPageContent.kt created!

--- app/src/main/java/com/example/migros_ui/adapter/ViewPagerAdapterForMainPageContent.kt
+++ app/src/main/java/com/example/migros_ui/adapter/ViewPagerAdapterForMainPageContent.kt
@@ -1,6 +1,8 @@
 package com.example.migros_ui.adapter
 
+import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class ViewPagerAdapterForMainPageContent{
+class ViewPagerAdapterForMainPageContent(fragment: Fragment):FragmentStateAdapter(fragment){
+    
 }
